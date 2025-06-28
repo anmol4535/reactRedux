3a38834acbda904c890810ca8660d0b82cc2fda1
Anmol Mishra <anmolmishra56113@gmail.com>
2025-06-28 18:22:23 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from './components/Navbar';
+import {Routes,Route} from "react-router-dom"
+import Cart from "./pages/Cart";
+import Home  from './pages/Home';
+
+function App() {
+  return (
+    <div >
+      <div className='bg-slate-900'>
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Spinner.css"
+
+const Spinner = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="spinner"></div>
+        </div>
+    )
+}
+export default Spinner;
